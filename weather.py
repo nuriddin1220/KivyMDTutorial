45ad476c40ec069591b137f380fbff9a074b0838
Sharipov Nuriddin <sharipov.n1220@gmail.com>
2023-05-18 20:13:24 +0500

    I committed before adding navigation Drawer

--- weather.py
+++ weather.py
@@ -5,8 +5,6 @@ from kivymd.uix.list import IconLeftWidget,TwoLineIconListItem
 from kivymd.uix.card import MDCard
 from kivy.metrics import dp
 from kivy.uix.popup import Popup
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
 from kivymd.uix.button import MDFloatingActionButton
 from kivy_garden.mapview import MapView ,MapMarker
 from kivymd.uix.gridlayout import MDGridLayout
@@ -70,9 +68,50 @@ class MainApp(MDApp):
             line_item =TwoLineIconListItem(
                    IconLeftWidget(icon="office-building-marker"),
                     text=item['hudud_nomi'],
-                    secondary_text=item['shahar_nomi']
+                    secondary_text=item['shahar_nomi'],
+                    on_release=lambda x, item=item: self.on_list_item_release(item)
             )
             hudud_list.add_widget(line_item)
+
+
+    def on_list_item_release(self,item):
+        print(item)
+        weather_data=get_weather(item['lat'],item['long'],self.appid)
+        print(weather_data)
+        if weather_data is not None:
+            print(weather_data)
+            icon_code=weather_data['weather'][0]['icon']
+            icon_url=f'https://openweathermap.org/img/wn/{icon_code}@2x.png'
+            description=weather_data['weather'][0]['description']
+            temperature=weather_data['main']['temp']
+            humidity=weather_data['main']['humidity']
+            visibility=weather_data['visibility'] #m
+            wind_speed=weather_data['wind']['speed'] #m/s
+            clouds=weather_data['clouds']['all']
+            city_name=weather_data['name']
+            data_time=datetime.datetime.fromtimestamp(weather_data['dt']).strftime('%B %d, %Y %H:%M:%S')
+            sunrise_time=datetime.datetime.fromtimestamp(weather_data['sys']['sunrise']).strftime('%B %d, %Y %H:%M:%S')
+            sunset_time=datetime.datetime.fromtimestamp(weather_data['sys']['sunset']).strftime('%B %d, %Y %H:%M:%S')
+            menu = MDScrollView(
+                            MDList(
+                                OneLineAvatarIconListItem(ImageLeftWidget(source=icon_url),text=f"{description}"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="temperature-celsius",theme_icon_color="Custom",icon_color="orange"),text=f"Temperature: {str(temperature)} Celsius"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="water-percent",theme_icon_color="Custom",icon_color="orange"),text=f"Humidity: {str(humidity)} %"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="eye",theme_icon_color="Custom",icon_color="orange"),text=f"Visibility: {str(visibility)}"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="wind-power",theme_icon_color="Custom",icon_color="orange"),text=f"Wind speed: {str(wind_speed)} M/S"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="wind-power",theme_icon_color="Custom",icon_color="orange"),text=f"Cloudiness: {str(clouds)} %"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="city",theme_icon_color="Custom",icon_color="orange"),text=f"City name: {str(city_name)}"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="clock-time-eight",theme_icon_color="Custom",icon_color="orange"),text=f"Data Time: {str(data_time)}"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="weather-sunset-up",theme_icon_color="Custom",icon_color="orange"),text=f"Sunrise Time: {str(sunrise_time)}"),
+                                OneLineAvatarIconListItem(IconLeftWidget(icon="weather-sunset-down",theme_icon_color="Custom",icon_color="orange"),text=f"Sunset Time: {str(sunset_time)}")))
+            popup = Popup(content=menu,title=f"Information",auto_dismiss=True,pos_hint = {'center_x':0.5,'top':0.9},size_hint=(0.6,0.8))
+            popup.content=menu
+            popup.open()
+        else:
+            menu=MDLabel(text='Try Again:Network issue!',halign="center",size_hint=(1,1))
+            popup = Popup(content=menu,title=f"Error",auto_dismiss=True,pos_hint = {'center_x':0.5,'center_y':0.5},size_hint=(0.4,0.2))
+            popup.open()
+
 
     def loc_released(self):
         geo_data=get_current_location()
@@ -207,7 +246,6 @@ class MainApp(MDApp):
                 menu = MDScrollView(
                                 MDList(
                                     OneLineAvatarIconListItem(ImageLeftWidget(source=icon_url),text=f"{description}"),
-                                    OneLineAvatarIconListItem(IconLeftWidget(icon="information"),text="Temperature:{temperature}"),
                                     OneLineAvatarIconListItem(IconLeftWidget(icon="temperature-celsius",theme_icon_color="Custom",icon_color="orange"),text=f"Temperature: {str(temperature)} Celsius"),
                                     OneLineAvatarIconListItem(IconLeftWidget(icon="water-percent",theme_icon_color="Custom",icon_color="orange"),text=f"Humidity: {str(humidity)} %"),
                                     OneLineAvatarIconListItem(IconLeftWidget(icon="eye",theme_icon_color="Custom",icon_color="orange"),text=f"Visibility: {str(visibility)}"),
@@ -220,26 +258,30 @@ class MainApp(MDApp):
                 popup = Popup(content=menu,title=f"Information",auto_dismiss=True,pos_hint = {'center_x':0.5,'top':0.9},size_hint=(0.6,0.8))
                 popup.content=menu
                 popup.open()
+            else:
+                menu=MDLabel(text='Try Again:Network issue!',halign="center",size_hint=(1,1))
+                popup = Popup(content=menu,title=f"Error",auto_dismiss=True,pos_hint = {'center_x':0.5,'center_y':0.5},size_hint=(0.4,0.2))
+                popup.open()
 
 
     def map_button_released(self):
-        if self.custom_loc[0]=="00.0000":
-            geo_data=get_current_location()
-            if geo_data is None:
-                self.root.ids.map_label.text="Error:Getting GeoInfo data"
-            else:
-                self.root.ids.map_label.text="Creating Map . . ."
-                self.custom_loc=[geo_data[0],geo_data[1]]
-                try:
-                    mapview = MapView(zoom=11, lat=float(geo_data[0]), lon=float(geo_data[1]))
-                    mapview.bind(on_touch_down=self.on_map_touch)
-                    mapview.add_marker(MapMarker(lat=float(geo_data[0]), lon=(geo_data[1])))
-                    self.root.ids.map_screen.add_widget(mapview)
-                    close_button=MDFloatingActionButton(icon="close-circle",type='small',text_color="#211c29",theme_icon_color="Custom",pos_hint={'right':0.99, 'top':0.99})
-                    self.root.ids.map_screen.add_widget(close_button)
-                    print(dir(mapview))
-                    self.root.ids.map_label.text=""
-                except:
-                    self.root.ids.map_label.text="Try again! Map creation failed!"
+        geo_data=get_current_location()
+        if geo_data is None:
+            self.root.ids.map_label.text="Try Again:Getting GeoInfo data"
+        else:
+            self.root.ids.map_label.text="Creating Map . . ."
+            self.custom_loc=[geo_data[0],geo_data[1]]
+            try:
+                mapview = MapView(zoom=11, lat=float(geo_data[0]), lon=float(geo_data[1]))
+                mapview.bind(on_touch_down=self.on_map_touch)
+                mapview.add_marker(MapMarker(lat=float(geo_data[0]), lon=(geo_data[1])))
+                self.root.ids.map_screen.add_widget(mapview)
+                # close_button=MDFloatingActionButton(icon="close-circle",type='small',text_color="#211c29",theme_icon_color="Custom",pos_hint={'right':0.99, 'top':0.99})
+                # self.root.ids.map_screen.add_widget(close_button)
+                # print(dir(mapview))
+                self.root.ids.map_label.text=""
+            except:
+                self.root.ids.map_label.text="Try again! Map creation failed!"
+                
 
 MainApp().run()
